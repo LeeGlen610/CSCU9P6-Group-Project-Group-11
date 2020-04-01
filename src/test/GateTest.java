56c7f21c6140d5c69673ae96d32ad880847193bc
JonL1207 <jonlawuk@yahoo.co.uk>
2020-04-01 22:25:48 +0100

    Improved the test classes for GateInfoDatabaseTest and Gate, threw exceptions for when an attempt to change the status of the gate fails (makes unit testing easier), and fixed GateConsole so it works

--- src/test/GateTest.java
+++ src/test/GateTest.java
@@ -1,42 +1,96 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GateTest {
 
     private Gate gate;
+    private final int mCode = 123;
 
     @Before
-    public void setUp() {
+    public void before() {
         gate = new Gate();
     }
 
     @Test
     public void testCreate(){
-        assertNotNull("Gate not created properly", gate);
+        assertNotNull(gate, "Gate not created properly");
     }
 
     @Test
     public void getStatus() {
-        assertEquals("Gate initial status should be free(0)", 0, gate.getStatus());
+        assertEquals(0, gate.getStatus(), "Gate initial status should be free(0)");
     }
 
     @Test
     public void allocate() {
-        gate.allocate(1);
-        assertEquals("Gate status should be reserved(1)", 1, gate.getStatus());
+        gate.allocate(mCode);
+
+        assertEquals(1, gate.getStatus(), "Gate status should be reserved(1)");
+    }
+
+    @Test
+    public void allocateWhenReserved() {
+        gate.allocate(mCode);
+
+        assertThrows(IllegalStateException.class, () -> gate.allocate(mCode), "Attempt to allocate when gate status is reserved(1), and fails");
+    }
+
+    @Test
+    public void allocateWhenOccupied() {
+        gate.allocate(mCode);
+        gate.docked();
+
+        assertThrows(IllegalStateException.class, () -> gate.allocate(mCode), "Attempt to allocate when gate status is occupied(2), and fails");
     }
 
     @Test
     public void docked() {
+        gate.allocate(mCode);
         gate.docked();
-        assertEquals("Gate status should be occupued(2)", 2, gate.getStatus());
+
+        assertEquals(2, gate.getStatus(), "Gate status should be occupied(2)");
+    }
+
+    @Test
+    public void dockWhenFree() {
+        assertThrows(IllegalStateException.class, () -> gate.docked(), "Attempt to dock when gate status is free(0), and fails");
+    }
+
+    @Test
+    public void dockWhenOccupied() {
+        gate.allocate(mCode);
+        gate.docked();
+
+        assertThrows(IllegalStateException.class, () -> gate.docked(), "Attempt to dock when gate status is occupied(2), and fails");
     }
 
     @Test
     public void departed() {
+        gate.allocate(mCode);
+        gate.docked();
         gate.departed();
-        assertEquals("Gate status should be free(0)", 0, gate.getStatus());
+
+        assertEquals(0, gate.getStatus(), "Gate status should be free(0)");
+    }
+
+    @Test
+    public void departWhenFree() {
+        assertThrows(IllegalStateException.class, () -> gate.departed(), "Attempt to depart when gate status is free(0), and fails");
+    }
+
+    @Test
+    public void departWhenReserved() {
+        gate.allocate(mCode);
+
+        assertThrows(IllegalStateException.class, () -> gate.departed(), "Attempt to depart when gate status is reserved(1), and fails");
+    }
+
+    @Test
+    public void getmCode(){
+        gate.allocate(mCode);
+
+        assertEquals(123, gate.getmCode(), "Gate mCode should be 123");
     }
 }
